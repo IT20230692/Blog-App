fbf51feb15e241cc4fd16e779d9e0d0401e70b13
IT20230692 <IT20230692@my.sliit.lk>
2023-01-03 19:37:04 +0530

    create register forntend page

---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,21 @@
+import "./register.css"
+
+export default function Register() {
+  return (
+    <div className='register'>
+    <span className='registerTitle'>Register</span> 
+    <form className="registerForm">
+        <label>Usrename</label>
+        <input type="username" className="registerInput" placeholder="Enter Your Username..." name="email"/>
+        <label>E-mail</label>
+        <input type="email" className="registerInput" placeholder="Enter Your E-mail..." name="email"/>
+        <label>Password</label>
+        <input type="password" className="registerInput" placeholder="Enter Your Password..."/>
+        <label>Re-Password</label>
+        <input type="password" className="registerInput" placeholder="Re-Enter Your Password..."/>
+      <button className="registerButton">Register</button>
+    </form>
+    <button className="registerLoginButton">Login</button>
+    </div>
+  );
+}
