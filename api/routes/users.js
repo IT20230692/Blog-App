6690c240693cda7388ed58cbcd97ca9202e074cb
IT20230692 <IT20230692@my.sliit.lk>
2023-01-10 16:46:27 +0530

    create delete user

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const Post = require('../models/Post');
 const bcrypt = require('bcrypt');
 
 //update
@@ -23,6 +24,47 @@ router.put('/:id', async (req, res) => {
     }
   } else {
     return res.status(401).json('You can update your account only...');
+  }
+});
+
+
+//delete
+// router.delete('/:id', async (req, res) => {
+//   if (req.body.userId === req.params.id) {
+//    try{
+//       const user = await User.findById(req.params.id);
+//     try {
+//       await Post.deleteMany({ username: user.username});
+//       await User.findByIdAndDelete(req.params.id);
+//       res.status(200).json("user has been deleted...");
+//    } catch(err){
+//       res.status(500).json(err);
+//     } catch (err) {
+//       return res.status(404).json("User Not Found!");
+//     }
+//   } else {
+//     return res.status(401).json('You can delete your account only...');
+//   }
+// });
+
+//delete
+router.delete('/:id', async (req,res) =>{
+  if(req.body.userId === req.params.id){
+    try{
+      const user = await User.findById(req.params.id);
+      try{
+        await Post.deleteMany({ username: user.username});
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("User deleted successfully...");
+      }catch (err){
+        res.status(500).json(err);
+      }
+    } catch (err){
+      res.status(404).json("user not found!");
+    }
+  }else{
+   return res.status(401).json('You can delete your account only...');
+
   }
 });
 
