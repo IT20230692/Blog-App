43292eb247fac28fe311d7936d86427638166cda
IT20230692 <IT20230692@my.sliit.lk>
2023-01-13 17:37:05 +0530

    update home forntend

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,8 +2,19 @@ import "./home.css";
 import Header from "../../components/header/Header";
 import Posts from "../../components/posts/Posts";
 import SideBar from "../../components/sideBar/SideBar";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [posts,setPosts] = useState([]);
+
+  useEffect(()=>{
+      const fetchPosts = async ()=>{
+      const res =  await axios.get("/posts")
+      console.log(res);
+    }
+    fetchPosts();
+  },[])
   return (
     <>
     <Header/>
@@ -14,3 +25,4 @@ export default function Home() {
     </>
   );
 }
+ 
