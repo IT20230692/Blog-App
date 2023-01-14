32524b2377b492434abfb7e3d6b0f762eb8dd0fd
IT20230692 <IT20230692@my.sliit.lk>
2023-01-15 01:13:23 +0530

    create singlePost page retrive option

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -1,42 +1,47 @@
-import "./singlePost.css"
+import { useLocation } from "react-router";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./singlePost.css";
 
 export default function SinglePost() {
-  
+  const location = useLocation();
+  const path = location.pathname.split("/")[2];
+  const [post, setPosts] = useState({})
+
+  useEffect(() => {
+    const getPost = async ()=>{
+      const res = await axios.get("/posts/" + path);
+      setPosts(res.data);
+    };
+    getPost();
+  },[path])
   return (
     <div className="singlePost">
       <div className="singlePostWrapper">
+      {post.photo && (
+
         <img 
         className="singlePostImg"
-        src="https://upload.wikimedia.org/wikipedia/commons/d/d0/German_Shepherd_-_DSC_0346_%2810096362833%29.jpg"
+        src={post.photo}
           alt="imgview"
         /> 
+
+      ) }
+        
         <h1 className="singlePostTitle">
-        German Shepherd
+       {post.title}
         <div className="singlePostEdit">
         <i className="singlePostIcon far fa-edit"></i>
         <i className="singlePostIcon far fa-trash-alt"></i>
         </div>
         </h1>
         <div className="singlePostInfo">
-        <span className="singlePostAuthor">Author : <b>Chandima</b></span>
-        <span className="singlePostDate">1 hour ago</span>
+        <span className="singlePostAuthor">Author : <b>{post.username}</b></span>
+              {new Date(post.createdAt).toDateString()}
+        <span className="singlePostDate"></span>
         </div>
         <p className="singlePostDesc">
-        The German Shepherd is a larger than average dog breed that
-          originated in Germany. A breed of dog that obeys its master.
-           It is the third most intelligent breed of dog in the world. 
-           The German Shepherd is a larger than average dog breed that
-          originated in Germany. A breed of dog that obeys its master.
-           It is the third most intelligent breed of dog in the world. 
-           The German Shepherd is a larger than average dog breed that
-          originated in Germany. A breed of dog that obeys its master.
-           It is the third most intelligent breed of dog in the world. 
-           The German Shepherd is a larger than average dog breed that
-          originated in Germany. A breed of dog that obeys its master.
-           It is the third most intelligent breed of dog in the world.
-            The German Shepherd is a larger than average dog breed that
-          originated in Germany. A breed of dog that obeys its master.
-           It is the third most intelligent breed of dog in the world. 
+        {post.desc}
         </p>
       </div>
     </div>
